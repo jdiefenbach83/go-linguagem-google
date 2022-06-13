1d61578f1d3cb97abdd0f6ae31c016f8113e3ce9
Jefferson Diefenbach <jefferson.diefenbach@gmail.com>
2022-06-12 21:19:24 -0300

    Aula 02 - Variáveis

--- src/hello/hello.go
+++ src/hello/hello.go
@@ -3,6 +3,19 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello World")
-	fmt.Println("First program in Go")
+	name := "Jefferson"
+	version := 1.2
+
+	fmt.Println("Hello", name)
+	fmt.Println("Program version", version)
+
+	fmt.Println("1 - Inicial monitoramento")
+	fmt.Println("2 - Exibir logs")
+	fmt.Println("0 - Sair do programa")
+
+	var command int
+	fmt.Scan(&command)
+
+	fmt.Println("Variable command address", &command)
+	fmt.Println("Chosen command: ", command)
 }
