f2b644209044a79de506b8b8ef8129bf5278a11d
Jefferson Diefenbach <jefferson.diefenbach@gmail.com>
2022-06-14 09:31:07 -0300

    Aula 03 - Controlando o fluxo do script

--- src/hello/hello.go
+++ src/hello/hello.go
@@ -1,21 +1,45 @@
 package main
 
 import "fmt"
+import "os"
 
 func main() {
+	showIntroduction()
+	showMenu()
+	command := readCommand()
+
+	switch command {
+	case 1:
+		fmt.Println("Monitorando...")
+	case 2:
+		fmt.Println("Exibindo Logs...")
+	case 0:
+		fmt.Println("Saindo do programa")
+		os.Exit(0)
+	default:
+		fmt.Println("Não conheço esse comando")
+		os.Exit(-1)
+	}
+}
+
+func showIntroduction() {
 	name := "Jefferson"
 	version := 1.2
 
 	fmt.Println("Hello", name)
 	fmt.Println("Program version", version)
+}
 
+func showMenu() {
 	fmt.Println("1 - Inicial monitoramento")
 	fmt.Println("2 - Exibir logs")
 	fmt.Println("0 - Sair do programa")
+}
 
-	var command int
-	fmt.Scan(&command)
+func readCommand() int {
+	var readCommand int
+	fmt.Scan(&readCommand)
+	fmt.Println("Chosen command: ", readCommand)
 
-	fmt.Println("Variable command address", &command)
-	fmt.Println("Chosen command: ", command)
+	return readCommand
 }
