b5e8cc6f4410b46c9ed676a0781ac1ef8dfad0f1
Jefferson Diefenbach <jefferson.diefenbach@gmail.com>
2022-06-15 08:51:40 -0300

    Aula 04 - Fazendo requisições para a web

--- src/hello/hello.go
+++ src/hello/hello.go
@@ -1,24 +1,30 @@
 package main
 
-import "fmt"
-import "os"
+import (
+	"fmt"
+	"net/http"
+	"os"
+)
 
 func main() {
 	showIntroduction()
-	showMenu()
-	command := readCommand()
 
-	switch command {
-	case 1:
-		fmt.Println("Monitorando...")
-	case 2:
-		fmt.Println("Exibindo Logs...")
-	case 0:
-		fmt.Println("Saindo do programa")
-		os.Exit(0)
-	default:
-		fmt.Println("Não conheço esse comando")
-		os.Exit(-1)
+	for {
+		showMenu()
+		command := readCommand()
+
+		switch command {
+		case 1:
+			startMonitoring()
+		case 2:
+			fmt.Println("Exibindo Logs...")
+		case 0:
+			fmt.Println("Saindo do programa")
+			os.Exit(0)
+		default:
+			fmt.Println("Não conheço esse comando")
+			os.Exit(-1)
+		}
 	}
 }
 
@@ -42,4 +48,17 @@ func readCommand() int {
 	fmt.Println("Chosen command: ", readCommand)
 
 	return readCommand
+}
+
+func startMonitoring() {
+	fmt.Println("Monitorando...")
+	site := "https://www.alura.com.br"
+	response, _ := http.Get(site)
+
+	if response.StatusCode == 200 {
+		fmt.Println("Site: ", site, " was loaded with success!")
+	} else {
+		fmt.Println("Site: ", site, " has problems! Status code:", response.StatusCode)
+	}
+
 }
